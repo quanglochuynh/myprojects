d111a803029fb406dbf1eb6fa330ad4c6dd15cbb
quanglochuynh <quanglochuynh@gmail.com>
2021-02-16 13:40:34 +0700

    fix some bugs

--- LuckSimulation/luck_simulator.cpp
+++ LuckSimulation/luck_simulator.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <iomanip>
 using namespace std;
 
-const int sampleSize = 10000;
+const int sampleSize = 1000;
 const int chosenNum = 20;
-float strength[sampleSize], luck[sampleSize];
-float result[sampleSize];
+const float luckContribution = 0.05;        //percentage of luck on the overall evaluation (e.g: 5% ->strength = 1 - 5% = 95%)
+
+struct people{
+    float strength;
+    float luck;
+    float result;
+};
+
+people array[sampleSize];
+float sumOfStrength=0, sumOfLuck = 0;
 
 void initArray(){
   srand(time(NULL));
   for (int i=0; i < sampleSize; i++){
-    strength[i] = (rand()%10000)/(float)100;
-    luck[i] = (rand()%10000)/(float)100;
-    result[i] = ((strength[i] * 0.95) + (luck[i] * 0.05));
+    array[i].strength = (rand()%10000)/(float)100;
+    array[i].luck = (rand()%10000)/(float)100;
+    array[i].result = ((array[i].strength * (1-luckContribution)) + (array[i].luck * luckContribution));
+    sumOfStrength += array[i].strength;
+    sumOfLuck += array[i].luck;
   }
 }
 
 void swapRes(int a, int b){
-  float temp = result[a];
-  result[a] = result[b];
-  result[b] = temp;
-
-  temp = strength[a];
-  strength[a] = strength[b];
-  strength[b] = temp;
-
-  temp = luck[a];
-  luck[a] = luck[b];
-  luck[b] = temp;
+  people temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
 }
 
-
-
-void sort(){
+void sort(){        //buble sort because it's simple
   for (int i=0; i<sampleSize; i++){
     for (int j=i+1; j<=sampleSize; j++){
-      if (result[i] < result[j]){
+      if (array[i].result < array[j].result){
         swapRes(i,j);
       }
     }
@@ -48,12 +47,14 @@ void printRes(int num) {
     int unlucky=0;
     for (int i=0; i<num; i++){
         cout << i+1 << ": \n";
-        cout << "Result: " << result[i]<< endl;
-        cout << "Strength: " << strength[i] << endl;
-        cout << "Luck: " << luck[i] << endl << endl << endl;
-        if (luck[i]<50) unlucky++;
+        cout << "Result: " << array[i].result<< endl;
+        cout << "Strength: " << array[i].strength << endl;
+        cout << "Luck: " << array[i].luck << endl << endl << endl;
+        if (array[i].luck<50) unlucky++;
     }
     cout << "Number of unlucky people: " << unlucky<< endl;
+    cout << "Average strength: " << sumOfStrength/sampleSize << endl;
+    cout << "Average luck: " << sumOfLuck / sampleSize << endl;
 }
 
 int main() {
