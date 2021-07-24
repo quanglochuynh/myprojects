a2fc6a10bcf5a1df1c4976d79ae2fc3ee38756a2
Loc Huynh <quanglochuynh@gmail.com>
2021-07-24 14:13:35 +0700

    fix bigs (alot)

--- Graphic/p5js/Flappy_bird/project.js
+++ Graphic/p5js/Flappy_bird/project.js
@@ -1,19 +1,129 @@
-// Single-sketch example
+let numOfpole = 3;
+let poleSpacing = 200;
+let poleWidth = 80
+let hole = 300;
+let birdY;
+let birdX = 150;
+let vs = 0;
+let a = 1;
+let t=0;
+let allow = true;
+let play = false;
+let lose = false;
+
+class pole{
+    constructor(x){
+        this.x = x;
+        this.y = Math.floor(random(hole,height));
+        this.w = poleWidth;
+        this.h = height - this.y;   
+    }
+}
+
+let polePosition = [pole];
 
 function setup (){
-    createCanvas (800, 600);
+    createCanvas (600, 800);
+    noStroke();
+    reset();
 }
-  
-function draw(){
+
+function check() {
+    if (birdY > height || birdY < 0){
+        lose = true;
+    }
+    let px = polePosition[0].x;
+    let py = polePosition[0].y;
+    if ((birdX < px) && ((birdY > py) || (birdY < py - hole))){
+        //s1
+        if (birdX + 25 >= px){
+            lose = true;
+            console.log('s1');
+        }
+    }else if ((birdY < py) && (birdX < px)){
+        //s2
+        if ((dist(birdX, birdY, px, py) < 25) || (dist(birdX, birdY, px, py+hole) < 25)){
+            lose = true
+            console.log('s2');
+        }
+    }else if ((birdY < py) && (birdX > px) && (birdX < px + poleWidth)){
+        //s3
+        if ((birdY +25 > py) || (birdY-25 < py -hole)){
+            lose = true;
+            console.log('s3');
+        }
+    }else if (birdX > px + poleWidth){
+        //s4
+        if ((dist(birdX, birdY, px+poleWidth, py) < 25) || (dist(birdX, birdY, px+poleWidth, py - hole)< 25)){
+            lose = true;
+            console.log('s4');
+        }
+    }
+
+}
+
+function updatePosition(){
+    for (let i=0; i<polePosition.length; i++){
+        polePosition[i].x -= 2;
+    }
+    if (polePosition[0].x + poleWidth < 0){
+        for (let j=0; j< numOfpole -1; j++){
+            polePosition[j].x = polePosition[j+1].x;
+            polePosition[j].y = polePosition[j+1].y;
+            polePosition[j].w = polePosition[j+1].w;
+            polePosition[j].h = polePosition[j+1].h;
+        }
+        let t = polePosition[numOfpole-2].x + poleWidth + poleSpacing;
+        polePosition[numOfpole-1] = new pole(t);
+    }
+    vs += a;
+    birdY += vs;
+    check();
+}
+
+function drawPole(){
+    for (let i=0; i<polePosition.length; i++){
+        rect(polePosition[i].x, polePosition[i].y, polePosition[i].w, polePosition[i].h);
+        rect(polePosition[i].x, 0, poleWidth, height-polePosition[i].h - hole);
+    }
+    ellipse(birdX,birdY, 50, 50);
+}
+
+function reset() {
     background(50);
     fill(255);
-    noStroke();
-    rectMode(CENTER);
-    if (mouseX > width/2){
-        fill(255,100,200);
+    //INITIALIZATION
+    birdY = height/2;
+    for (let i=0; i < numOfpole; i++){
+        polePosition[i] = new pole(width + 100 + i*(poleSpacing+poleWidth));
     }
-    else {
-        fill(255)
-    }
-    ellipse(mouseX, mouseY, 50, 50);
+    ellipse(birdX, birdY, 50, 50);
+    play = false;
+    allow = true;
+    lose = false;
 }
+
+function draw(){
+    t++;
+    if (t%10 ==0){
+        allow = true;
+    }
+    if (play && !lose){
+        background(50);
+        drawPole();
+        if ((keyIsPressed == true && allow)){
+            //console.log(birdY);
+            vs = -15;
+            allow = false
+        }
+        updatePosition();
+    }else if (keyIsPressed && !lose && allow){
+        play = true;
+        vs = -10 ;
+    }else if (lose){
+        if (keyIsPressed && allow){
+            reset();
+        }
+    }
+}
+
