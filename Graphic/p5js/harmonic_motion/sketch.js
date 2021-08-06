e3510d16380c7a3f8b9c8c6863907e6f14c4f4c0
Loc Huynh <quanglochuynh@gmail.com>
2021-08-06 19:28:44 +0700

    .

--- Graphic/p5js/harmonic_motion/sketch.js
+++ Graphic/p5js/harmonic_motion/sketch.js
@@ -1,6 +1,23 @@
+let x = 0;
+let a = 0;
+let v = 10;
 
+function drawParticle(x){
+    ellipse(width/2 + x, height/2, 10);
+}
 
 function setup(){
     createCanvas(600,600);
     background(50);
+}
+
+function draw(){
+    background(0);
+    a = -x*0.01;
+    v += a;
+    x += v;
+    
+    
+    console.log(v);
+    drawParticle(x);
 }
