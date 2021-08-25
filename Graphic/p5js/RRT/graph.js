b0fb9341db4b48bf374a52fd3cd17940f004a030
Loc Huynh <quanglochuynh@gmail.com>
2021-08-25 09:34:45 +0700

    .

--- Graphic/p5js/RRT/graph.js
+++ Graphic/p5js/RRT/graph.js
@@ -12,6 +12,15 @@ class Edge{
     }
 }
 
+function angle(x,y){
+    if (x<0){
+        return (Math.atan(y/x) + PI);
+    }else{
+        return Math.atan(y/x);
+    }
+}
+
+
 class Tree{
     constructor(nodes, edges, start){
         this.node = nodes;
@@ -19,6 +28,7 @@ class Tree{
         this.n = 1;
         this.start = start;
         this.trace = [];
+        this.distance = [0];
     }
 
     show(){
@@ -29,32 +39,69 @@ class Tree{
         }
         stroke('ORANGE');
         for (let i in this.edge){
-            line(this.node[this.edge[i].u].x, this.node[this.edge[i].u].y, this.node[this.edge[i].v].x, this.node[this.edge[i].v].y)
+            if (i!=0){
+                line(this.node[this.edge[i].u].x, this.node[this.edge[i].u].y, this.node[this.edge[i].v].x, this.node[this.edge[i].v].y);
+            }
         }
+    }
+
+    sampling(p){
+        let mindist = Number.MAX_SAFE_INTEGER;
+        let u=0;
+        for (let i in this.node){
+            let d = dist(this.node[i].x, this.node[i].y, p.x, p.y);
+            if ((d < mindist)){
+                mindist = d;
+                u=i;
+            }
+        }
+        let ang = angle(p.x - tree.node[u].x, p.y - tree.node[u].y);
+        //ellipse(v.x, v.y, 5);
+        return new Point(this.node[u].x + 20*Math.cos(ang), this.node[u].y + 20*Math.sin(ang));
     }
 
     addNode(nod, origin){
         this.node.push(nod);
         this.n++;
         this.edge.push(new Edge(origin, this.node.length-1));
-        this.trace[this.n-1]=origin;
+        this.trace[this.n-1] = origin;
+        this.distance[this.n-1] = dist(nod.x, nod.y, this.node[origin].x, this.node[origin].y);
+        //console.log(this.distance[this.n-1]);
     }
 
-    showTrace(){
+    optimizeSuround(rad){
+        let iv = this.n-1;
+        for (let i in this.node){
+            let d = dist(this.node[iv].x, this.node[iv].y, this.node[i].x, this.node[i].y);
+            if (d<rad){
+                if (this.distance[iv] + d < this.distance[i]){
+                    this.distance[i] = this.distance[iv] + d;
+                    this.trace[i] = iv;
+                }
+            }
+        }
+    }
+
+    showTrace(i){
         stroke('CYAN');
         let v;
-        let u = this.trace.length-1;
+        let u = i;
+        let dis=0;
         while(u != 0){
             v = this.trace[u];
             line(this.node[u].x, this.node[u].y, this.node[v].x, this.node[v].y);
+            dis += dist(this.node[u].x, this.node[u].y, this.node[v].x, this.node[v].y);
             u = v;
         }
+        console.log(dis)
+        return dis;
     }
+
+
 }
 
-
 class Map{
-    constructor(wid, hei, n, m, obstacle, start, destination){
+    constructor(wid, hei, n, m, obstacle, start, destination, bgc, oc){
         this.w = wid;
         this.h = hei;
         this.n = n;
@@ -64,9 +111,12 @@ class Map{
         this.destination = destination;
         this.margin = 50;
         this.spacing = min((this.w - 2*this.margin)/this.m, (this.h - 2*this.margin)/this.n);
+        this.bgc = bgc;
+        this.oc = oc
     }
 
     show(){
+        background(this.bgc);
         //draw grid
         for (let i=this.margin; i< this.margin+this.n*this.spacing; i+= this.spacing){
             stroke(100);
@@ -90,7 +140,7 @@ class Map{
         }
 
         //draw obstacles
-        fill(150);
+        fill(this.oc);
         noStroke();
         for(let i =0; i< this.obstacle.length; i++){
             rect(this.obstacle[i].x * this.spacing + this.margin, this.obstacle[i].y * this.spacing + this.margin, this.spacing, this.spacing)
@@ -105,6 +155,27 @@ class Map{
         strokeWeight(2);
         stroke(255);
         rect(this.margin, this.margin, this.m * this.spacing, this.n * this.spacing);
+    }
+
+    static getRandomPoint(){
+        let rw = random(graph.margin, graph.margin + graph.m*graph.spacing);
+        let rh = random(graph.margin, graph.margin + graph.n*graph.spacing);
+        fill('Cyan');
+        ellipse(rw, rh, 10);
+        return new Point(rw, rh);
+    }
+
+    checkForValidity(p){
+        if (this.hitObstacle(p) == false){
+            return false;
+        }
+        for (let i in this.obstacle){
+            let p = new Point(this.obstacle[i].x * this.spacing + this.margin, this.obstacle[i].y * this.spacing + this.margin);
+            if (this.isBlocked(p , v)){
+                return false;
+            }
+        }
+        return true;
     }
 
     hitObstacle(p){
--- Graphic/p5js/RRT/rrt_star.js
+++ Graphic/p5js/RRT/rrt_star.js
@@ -1,8 +1,15 @@
 let graph;
 let tree;
 let stage = true;
+let endv;
+let found = false;
+let bestDist = Number.MAX_SAFE_INTEGER;
+let backgroundColor;
+let obstacleColor;
 
 function init(){
+    backgroundColor = color(0);
+    obstacleColor = color(255);
     let s = new Point(8, 2);
     let d = new Point(1, 7);
     let obstacle = [];
@@ -17,7 +24,7 @@ function init(){
     obstacle.push(new Point(5,8));
     obstacle.push(new Point(3,9));
     obstacle.push(new Point(5,5));
-    graph = new Map(width, height, 10, 10, obstacle, s, d);
+    graph = new Map(width, height, 10, 10, obstacle, s, d, backgroundColor, obstacleColor);
     let node = [new Point(s.x * graph.spacing + graph.margin + graph.spacing/2, s.y * graph.spacing + graph.margin + graph.spacing/2)];
     let edges = [new Edge(0,1)];
     tree = new Tree(node, edges, node[0]);
@@ -25,7 +32,7 @@ function init(){
 
 function setup(){
     createCanvas(600,600);
-    frameRate(60);
+    frameRate(50);
     background(0);
     init();
     
@@ -39,57 +46,67 @@ function nearest(x,y){
     let mindist = Number.MAX_SAFE_INTEGER;
     let u=0;
     for (let i in tree.node){
-        let d = dist(tree.node[i].x, tree.node[i].y, x, y);
-        if (d < mindist){
-            mindist = d;
-            u=i;
+        if (found){
+            if (i == endv){
+                
+            }else{
+                let d = dist(tree.node[i].x, tree.node[i].y, x, y);
+                if ((d < mindist)){
+                    mindist = d;
+                    u=i;
+                }
+            }
+        }else{
+            let d = dist(tree.node[i].x, tree.node[i].y, x, y);
+            if ((d < mindist)){
+                mindist = d;
+                u=i;
+            }
         }
     }
     return u;
 }
 
-function angle(x,y){
-    if (x<0){
-        return (Math.atan(y/x) + PI);
-    }else{
-        return Math.atan(y/x);
-    }
-}
 
 function draw(){
+    let v;
+    if (mouseIsPressed){
+        stage = false;
+    }
     if (stage){
-        background(0);
         graph.show();
         //random node
-        let rw = round(random(graph.margin, graph.margin + graph.m*graph.spacing));
-        let rh = round(random(graph.margin, graph.margin + graph.n*graph.spacing));
-        fill('Cyan');
-        ellipse(rw, rh, 10);
-        let u = nearest(rw,rh);
-        let ang = angle(rw - tree.node[u].x, rh - tree.node[u].y);
-        let v = new Point(tree.node[u].x + 20*Math.cos(ang), tree.node[u].y + 20*Math.sin(ang));
-        ellipse(v.x, v.y, 5);
-        //scan for obstacles
-        let add = true;
-        for (let i in graph.obstacle){
-            let x = graph.obstacle[i].x * graph.spacing + graph.margin;
-            let y = graph.obstacle[i].y * graph.spacing + graph.margin;
-            let p = new Point(x, y);
-            if (graph.isBlocked(p , v)){
-                add = false;
-                break;
-            }
-        }
-        if ((add==true) && !graph.hitObstacle(v)){
+        let randomPoint = graph.getRandomPoint();
+        
+
+        v = tree.sampling(randomPoint);
+
+        if (graph.checkForValidity(v)){
             tree.addNode(v,u);
         }
-        tree.show();
+
+        //tree.optimizeSuround(40);
+        
         //checking
         if (graph.reachDestination(v)){
-            stage = false;
-            tree.showTrace();
+
+
+
+            // if (tree.distance[tree.node.length -1] < bestDist){
+            //     bestDist = tree.distance[tree.node.length -1];
+            //     endv = tree.node.length -1;
+            //     //console.log(tree.distance[endv]);
+            // }
+            
+
+            if (tree.showTrace(endv)<450){
+                stage = false;
+            }
         }
+        tree.show();
     }else{
-        
+        tree.showTrace(endv);
+        console.log('DONE!!!');
+        console.log('Distance is ' + tree.distance[endv]);
     }
 }
