b03cd2febde50a46037f71b7439c9310a817b51b
Loc Huynh <quanglochuynh@gmail.com>
2021-08-27 10:42:26 +0700

    finishing

--- Graphic/p5js/RRT/rrt_star.js
+++ Graphic/p5js/RRT/rrt_star.js
@@ -1,38 +1,53 @@
 let graph;
 let tree;
-let stage = true;
+let stage = false;
 let endv;
 let found = false;
 let bestDist = Number.MAX_SAFE_INTEGER;
 let backgroundColor;
 let obstacleColor;
-let samplingDistance=40;
+let samplingDistance=30;
+let res;
+let matrix;
 
 function init(){
     backgroundColor = color(0);
     obstacleColor = color(150);
-    let s = new Point(8, 2);
+    let s = new Point(9, 0);
     let d = new Point(1, 7);
     let obstacle = [];
-    obstacle.push(new Point(5,0));
-    obstacle.push(new Point(3,1));
-    obstacle.push(new Point(5,2));
-    obstacle.push(new Point(3,3));
-    obstacle.push(new Point(5,4));
-    obstacle.push(new Point(3,5));
-    obstacle.push(new Point(5,6));
-    obstacle.push(new Point(3,7));
-    obstacle.push(new Point(5,8));
-    obstacle.push(new Point(3,9));
-    obstacle.push(new Point(5,5));
-    graph = new Map(width, height, 10, 10, obstacle, s, d, backgroundColor, obstacleColor);
+
+    let str = undefined;
+    for(let i=0; i < matrix.getRowCount(); i++){
+        for(let j = 0; j < matrix.getColumnCount(); j++){
+            str = matrix.getString(i,j);
+            console.log(str);
+            if (str == ''){
+                continue;
+            }
+            if (str = '1'){
+                obstacle.push(new Point(j,i));
+            }else if (str == '2'){
+                s = new Point(j,i);
+            }else if (str == '3'){
+                d = new Point(j,i);
+            }
+            
+        }
+    }
+
+    graph = new Map(width, height, matrix.getRowCount(), matrix.getColumnCount(), obstacle, s, d, backgroundColor, obstacleColor);
     let node = [new Point(s.x * graph.spacing + graph.margin + graph.spacing/2, s.y * graph.spacing + graph.margin + graph.spacing/2)];
     tree = new Tree(node, samplingDistance, height*width);
 }
 
+function preload(){
+    matrix = loadTable('test1.csv', 'csv');
+}
+
 function setup(){
-    createCanvas(600,600);
-    frameRate(5);
+    createCanvas(800,800);
+    frameRate(10);
     background(0);
     init();
 }
@@ -41,12 +56,16 @@ function draw(){
     if (mouseIsPressed){
         if (stage){
             stage = false;
-            tree.showTrace(tree.n -1);
-            console.log('DONE!!!');
-            console.log('Distance is ' + tree.distance[endv]);
+            if (found){
+                let res = tree.showPath(endv, color('CYAN'));
+                console.log('Distance is ' + res);
+            }
         }else{
             stage = true;
         }
+    }
+    if (tree.n>1000){
+        stage = false;
     }
     if (stage){
         graph.show();
@@ -55,14 +74,29 @@ function draw(){
         let v = tree.getSamplingPoint(randomPoint, nearestID);
         if (graph.checkForValidity(v)){
             tree.addNode(v, nearestID);
-        }
-        //tree.optimizeSuround(40);
-        if (graph.reachDestination(v)){
-            console.log('done');
-            stage = false;
+            //tree.adjustRadius();
+            tree.optimizeSuround2(samplingDistance*2);
         }
         tree.show();
+        if (graph.reachDestination(v)){
+            if (!found){
+                console.log('Found first path!');
+                found = true;
+                endv = tree.n-1;
+            }
+            res = tree.showPath(endv, color('CYAN'));
+            if (res < bestDist){
+                bestDist = res;
+                tree.distance[endv] = res;
+                endv = tree.n-1;
+            }           
+        }
+        if (found == true){
+            res = tree.showPath(endv, color('CYAN'));
+            tree.distance[endv] = res;
+            console.log('Best distance: ' + res + '     n = ' + tree.n + '      sampling radius = ' + tree.samplingrad);
+        }
     }else{
-       
+       //do nothing
     }
 }
