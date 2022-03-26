451e045aca84bdaa54672268fed118927556e837
Loc Huynh <quanglochuynh@gmail.com>
2022-03-26 11:08:54 +0700

    .

--- Python/Thao/GA.py
+++ Python/Thao/GA.py
@@ -2,9 +2,12 @@ import numpy as np
 import csv
 
 class Operation:
-    def __init__(self, name, starting_time, duration, machine):
+    def __init__(self, name, duration, machine):
         self.name = name
-        self.st = starting_time
+        self.es = 0
+        self.ef = 0
+        self.ls = 0
+        self.lf = 0
         self.duration = duration
         self.machine = machine
 
@@ -13,15 +16,17 @@ class Job:
         self.num_of_operation = num_of_ope
         self.data = data
 
-def calc_fitness():
-    return 0
-
 
 class DNA:
-    def __init__(self, sequence):
-        self.sequence = sequence
-        self.fitness = calc_fitness(sequence)
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.fitness = self.calc_fitness(matrix)
 
+    def calc_fitness(self, matrix):
+        for j in range(np.shape(matrix)[1]-1):
+            for i in range(np.shape(matrix)[0]):
+                print(str(matrix[i][j]) + " -> " + str(matrix[i][j+1]))
+        return 0;
         
 
 
@@ -32,20 +37,29 @@ job_array = []
 population = []
 population_size = 10
 
-# Doc file
-for k in range(3):
-    with open('job' + str(k+1) + '.csv') as csv_file:
-        data = []
-        i=0
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            i=i+1
-            data.append(row)
-        job_array.append(Job(i, data))
+# # Doc file
+# for k in range(3):
+#     with open('job' + str(k+1) + '.csv') as csv_file:
+#         data = []
+#         i=0
+#         csv_reader = csv.reader(csv_file, delimiter=',')
+#         for row in csv_reader:
+#             i=i+1
+#             data.append(row)
+#         job_array.append(Job(i, data))
 
-# print(job_array[1].data)
+# # print(job_array[1].data)
 
 
-for i in range(population_size):
-    #init DNA
-    population.append(DNA())
+# for i in range(population_size):
+#     #init DNA
+#     population.append(DNA())
+
+a = [[0, 1, 3, 4, 0, 0],
+         [0, 3, 4, 1, 2, 0],
+         [0, 4, 3, 3, 0, 0]]
+
+print(a)
+
+k = DNA(a)
+
