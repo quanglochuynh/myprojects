769a24b422a140013994562203dafb9dbaa1831b
Loc Huynh <quanglochuynh@gmail.com>
2022-05-09 12:14:45 +0700

    .

---
+++ Python/PSO/Logdes/pso.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#/ Objective
+# Min cost, max responsiveness, min emission
+# Zc, Zr, Ze
+# Objective function
+#minimize w1*Zc + w2*(M-Zr) +w3*Ze
+# 
+
+dc_name = ["Tonekabonl","Tonekabon2","	Tonekabon3","	Chalus1","	Chalus2","Chalus3","Qaemshahr1","Qaemshahr2","Ramsarl","Ramsar2","Baboll","Babol2","Neka"]
+garden_name = ["Ramsarl","Ramsar2","Ramsar3","Noorl","Noor2","Noor3","Saril","Sari2","Sari3"]
+fruit_customer_name = ["Chalus1","Chalus2","Babolsar","Amoll","Amol2","Amol3","Ramsar","Behshahr","Pol sefid"]
+compost_customer_name = ["Behshahr1","Behshahr2","Tonakabon","Neka","Juybar","Ramsar","Noor","Sari"]
+
+num_of_DC = len(dc_name)
+num_of_garden = len(garden_name)
+num_of_fruit_customer = len(fruit_customer_name)
+num_of_compost_customer = len(compost_customer_name)
+
+big_T = 8
+t_prime = 3
+fj2 = [36.363, 36.250, 31.818, 31.700, 30.800]
+fl = [18,181, 18,100, 13,636, 13,530, 12,520]
+dc = 0.073
+ch = 
+#Doc file
+
--- Python/PSO/polyphit.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def cost(fn, model):
-    sum = 0
-    for x in range():
-        val = np.polyval(fn, x)
-        sum += val^2
