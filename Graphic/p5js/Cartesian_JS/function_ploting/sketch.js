2fb6ebb75f39a5867405b8de8db0cf7539c9b734
Loc Huynh <quanglochuynh@gmail.com>
2022-08-02 20:31:22 +0700

    .

--- Graphic/p5js/Cartesian_JS/function_ploting/sketch.js
+++ Graphic/p5js/Cartesian_JS/function_ploting/sketch.js
@@ -4,6 +4,9 @@ function sigmoid(x){
     return 1/(1+Math.exp(-x));
 }
 
+// function poly(x):
+
+
 function setup(){
     createCanvas(1600,900);
     background(50);
@@ -14,6 +17,6 @@ function setup(){
 function draw(){
     background(10);
     plane.drawPlane();
-    plane.plot(sigmoid, 'orange');
-    plane.derivePlot(sigmoid, 'blue');
+    plane.plot(Math.sin, 'orange');
+    plane.derivePlot(Math.sin, 'blue');
 }
