aa518c25f0765f9375635404b302f3ea4a9acfce
Loc Huynh <quanglochuynh@gmail.com>
2022-08-11 13:21:21 +0700

    .

--- Graphic/p5js/Metaheuristic/PSO/sketch.js
+++ Graphic/p5js/Metaheuristic/PSO/sketch.js
@@ -1,51 +1,61 @@
-const pop_size = 100;
-const velMax = 8;
+const pop_size = 50;
+const velMax = 4;
 let w = 1;
-const c1 = 0.1;
-const c2 = 0.1;
-const wDamp = 0.9995;
+const c1 = 1;
+const c2 = 1;
+const wDamp = 0.995;
 let population = [];
 let gBest = undefined;
 let it = 0;
+let px = 0;
+let py = 0;
 
 function func(x,y){
-    return Math.pow(0.1*(x-50),2) - 100*Math.cos(0.02*Math.PI * (x-50)) + Math.pow(0.1*(y-50),2) - 100*Math.cos(0.02*Math.PI*(y-50)) + 200
+    return Math.pow(0.07*(x-px),2) - 100*Math.cos(0.02*Math.PI * (x-px)) + Math.pow(0.07*(y-py),2) - 100*Math.cos(0.02*Math.PI*(y-py)) + 200
+}
+
+function setMg(x, l){
+    let a = 0;
+    if (x._data[0] < 0){
+        a = atan(x._data[1]/x._data[0]) + Math.PI;
+    }else{
+        a = atan(x._data[1]/x._data[0]);
+    }
+    return math.matrix([l*cos(a), l*sin(a)]);
+}
+
+function mag(x){
+    return sqrt(sqr(x._data[0]) + sqr(x._data[1]));
 }
 class Particle{
     constructor(vec){
         if (vec!=undefined){
             this.position = vec;
         }else{
-            this.position = createVector(random(-width/2, width/2), random(-height/2, height/2));
+            this.position = math.matrix([random(-width/2, width/2), random(-height/2, height/2)]);
         }
-        this.velocity = createVector(random(-width/2, width/2), random(-height/2, height/2));
-        this.velocity.setMag(velMax);
+        this.velocity = math.matrix([random(-width/2, width/2), random(-height/2, height/2)]);
+        // this.velocity = setMg(this.velocity, velMax);
         this.pBest = undefined;
         this.fitness = 0;
     }
 
     calcFitness(){
-        // x = this.position.x;
-        // y = this.position.y;
-        // this.fitness = Math.pow(0.002*this.position.x,2) - 100*Math.cos(0.02*Math.PI * this.position.x) + Math.pow(0.002*this.position.y,2) - 100*Math.cos(0.02*Math.PI*this.position.y) + 200
-        // self.fitness = (self.dvar[0]**2 - 10 * np.cos(2 * np.pi * self.dvar[0])) + (self.dvar[1]**2 - 10 * np.cos(2 * np.pi * self.dvar[1])) + 20
-        // this.fitness = (Math.pow(this.position.x,2) - 10 ) + () + 20
-        this.fitness = func(this.position.x, this.position.y)
-        
+        this.fitness = func(this.position._data[0], this.position._data[1])
     }
 
     draw(){
-        circle(this.position.x, this.position.y, 10)
-        let head = p5.Vector.sub(this.position, p5.Vector.mult(this.velocity,8))
-        line(this.position.x, this.position.y, head.x, head.y)
+        circle(this.position._data[0], this.position._data[1], 10)
+        let head = math.subtract(this.position, math.multiply(this.velocity,8))
+        line(this.position._data[0], this.position._data[1], head.x, head.y)
     }
 
     updatePosition(){
-        this.position = p5.Vector.add(this.position, this.velocity)
-        this.position.x = min(width/2, this.position.x)
-        this.position.y = min(height/2, this.position.y)
-        this.position.x = max(-width/2, this.position.x)
-        this.position.y = max(-height/2, this.position.y)
+        this.position = math.add(this.position, this.velocity)
+        this.position._data[0] = min(width/2, this.position._data[0])
+        this.position._data[1] = min(height/2, this.position._data[1])
+        this.position._data[0] = max(-width/2, this.position._data[0])
+        this.position._data[1] = max(-height/2, this.position._data[1])
     }
 }
 
@@ -56,7 +66,7 @@ function init(){
     for (let i=0; i<pop_size; i++){
         population.push(new Particle())
         population[i].calcFitness()
-        population[i].pBest = new Particle(createVector(population[i].position));
+        population[i].pBest = new Particle();
         if (gBest.fitness > population[i].fitness){
             gBest = population[i]
         }
@@ -65,7 +75,7 @@ function init(){
 
 function setup(){
     createCanvas(800,800);
-    frameRate(60)
+    frameRate(30)
     background(50);
     translate(400, 400)
     fill(255)
@@ -89,26 +99,26 @@ function draw(){
     }
     translate(400, 400)
     background(50);
-    circle(50,50,10)
+    circle(px,py,10)
     for (let i=0; i<pop_size; i++){
         population[i].draw();
         // update vel
 
-        v0 = population[i].velocity.mult(w * random(1))
-        v1 = p5.Vector.sub(population[i].pBest.position, population[i].position).mult(random(1));
-        v2 = p5.Vector.sub(gBest.position, population[i].position).mult(random(1));
-        population[i].velocity = p5.Vector.add(v0, v1.mult(c1))
-        population[i].velocity = p5.Vector.add(population[i].velocity, v2.mult(c2))
+        v0 = math.multiply(population[i].velocity, w * random(1));
+        v1 = math.multiply(math.subtract(population[i].pBest.position, population[i].position),random(1));
+        v2 = math.multiply(math.subtract(gBest.position, population[i].position),random(1));
+        population[i].velocity = math.add(v0, math.multiply(v1,c1), math.multiply(v2,c2))
 
         // population[i].velocity.x = min(population[i].velocity.x, velMax)
         // population[i].velocity.y = min(population[i].velocity.y, velMax)
         // console.log(population[i].velocity.mag());
 
-        if (population[i].velocity.mag()>velMax){
-            population[i].velocity.setMag(velMax)
-        }
+        // if (mag(population[i].velocity)>velMax){
+        //     population[i].velocity = setMg(population[i].velocity, velMax)
+        // }
 
-        population[i].updatePosition()
+        // population[i].updatePosition()
+        population[i].position = math.add(population[i].position, population[i].velocity);
         
         population[i].calcFitness()
 
@@ -127,4 +137,15 @@ function draw(){
     fill(255)
     stroke(255)
     // noLoop()
+}
+
+function mousePressed(){
+    px = mouseX-width/2;
+    py = mouseY-height/2;
+    noLoop();
+    init();
+    loop();
+    
+    console.log(px + " " + py)
+
 }
