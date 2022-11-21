7f3bc0a107d8cf6d02d673bcac0fdd680bd791a5
Loc Huynh <quanglochuynh@gmail.com>
2022-11-21 13:26:14 +0700

    .

--- Graphic/p5js/PoseNet/sketch.js
+++ Graphic/p5js/PoseNet/sketch.js
@@ -3,6 +3,7 @@ let poseNet;
 let noseX = 0;
 let noseY = 0;
 let eye1X, eye1Y, eye2X, eye2Y;
+let poses = []; 
 
 function setup() {
   createCanvas(640, 480);
@@ -12,17 +13,18 @@ function setup() {
   poseNet.on('pose', gotPoses);
 }
 
-function gotPoses(poses) {
+function gotPoses(pose) {
+  poses = pose
   // console.log(poses);
   if (poses.length > 0) {
-    noseX = poses[0].pose.keypoints[0].position.x;
-    noseY = poses[0].pose.keypoints[0].position.y;
+    noseX = pose[0].pose.keypoints[0].position.x;
+    noseY = pose[0].pose.keypoints[0].position.y;
     
-    eye1X = poses[0].pose.keypoints[1].position.x;
-    eye1Y = poses[0].pose.keypoints[1].position.y;
+    eye1X = pose[0].pose.keypoints[1].position.x;
+    eye1Y = pose[0].pose.keypoints[1].position.y;
     
-    eye2X = poses[0].pose.keypoints[2].position.x;
-    eye2Y = poses[0].pose.keypoints[2].position.y;
+    eye2X = pose[0].pose.keypoints[2].position.x;
+    eye2Y = pose[0].pose.keypoints[2].position.y;
   }
 }
 
@@ -33,11 +35,11 @@ function modelReady() {
 function draw() {
   image(video, 0, 0);
 
-  fill(255, 0, 0);
-  ellipse(noseX, noseY, 50);
+  // fill(255, 0, 0);
+  // ellipse(noseX, noseY, 50);
   
-  eye(eye1X, eye1Y, 80, 1);
-  eye(eye2X, eye2Y, 80, -1);
+  // eye(eye1X, eye1Y, 80, 1);
+  // eye(eye2X, eye2Y, 80, -1);
 
   let eVec = createVector(eye2X-eye1X, eye2Y-eye1Y);
   let ang = eVec.heading();
@@ -53,6 +55,8 @@ function draw() {
   // circle(0,0,10);
   text("GDSC-IU",0,0)
   pop();
+  drawKeypoints();
+  drawSkeleton();
 }
 
 function eye(x, y, size, n) {
@@ -65,4 +69,39 @@ function eye(x, y, size, n) {
 	fill(56);
 	noStroke();
 	ellipse(x+cos(angle*n)*size/5, y+sin(angle*n)*size/5, size/2, size/2);
+}
+
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+      // ellipse(partA.position.x, partA.position.y, 15, 15);
+      // ellipse(partB.position.x, partB.position.y, 15, 15);
+
+    }
+  }
+}
+
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 5; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
 }
