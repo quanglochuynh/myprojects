4ab095c968f4ce214646ea536c606d43904902d5
Loc Q. Huynh <quanglochuynh@gmail.com>
2023-05-24 15:04:53 +0700

    .

--- Graphic/p5js/art/beenTogether/sketch.js
+++ Graphic/p5js/art/beenTogether/sketch.js
@@ -1,11 +1,11 @@
 const firstDate = 25;
 const firstMonth = 7;
 const firstYear = 2021;
-const dateOfMonth = [0,31,28,31,30,31,30,31,31,30,31,30,31];
+const dateOfMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 let total;
 let sec;
 
-function beenTogether(da, mo, yea){
+function beenTogether(da, mo, yea) {
   let d = firstDate;
   let m = firstMonth;
   let y = firstYear;
@@ -13,49 +13,64 @@ function beenTogether(da, mo, yea){
   do {
     d++;
     count++;
-    if (d>dateOfMonth[m]){
-        if ((y%4 == 0) && (m==2)){
-            count++;
-        }
-        d=1;
-        m++;
+    if (d > dateOfMonth[m]) {
+      if (y % 4 == 0 && m == 2) {
+        count++;
+      }
+      d = 1;
+      m++;
     }
-    if (m>12){
-      m=1;
+    if (m > 12) {
+      m = 1;
       y++;
     }
-    if ((d == da) && (m == mo) && (y == yea)){
-        return count;
+    if (d == da && m == mo && y == yea) {
+      return count;
     }
-  } while (true) ;
+  } while (true);
 }
 
 function setup() {
-    total = beenTogether(day(), month(), year());
-    createCanvas(600,600);
-    frameRate(30);
-    sec = second();
+  total = beenTogether(day(), month(), year());
+  createCanvas(displayHeight, displayHeight);
+  frameRate(30);
+  sec = second();
 }
 
-function draw(){
-    background('pink');
-    textAlign(CENTER, CENTER);
-    fill('white');
-    noStroke();
-    textSize(height/10);
-    text(total + ' days', width/2, height/2);
-    let sAngle = (sec + millis()/1000)* (2* Math.PI / 60) - Math.PI/2;
-    let mAngle = minute() * (2*Math.PI / 60) - Math.PI/2;
-    let hAngle = hour() * (2*Math.PI / 12) - Math.PI/2;
-    noFill();
-    strokeWeight(10);
-    stroke('white');
-    arc(width/2, height/2, width/2 + 80, height/2 + 80, -Math.PI/2, sAngle, OPEN);
-    strokeWeight(15);
+function draw() {
+  background("pink");
+  textAlign(CENTER, CENTER);
+  fill("white");
+  noStroke();
+  textSize(height / 10);
+  text(total + " days", width / 2, height / 2);
+  let sAngle = (sec + millis() / 1000) * ((2 * Math.PI) / 60) - Math.PI / 2;
+  let mAngle = minute() * ((2 * Math.PI) / 60) - Math.PI / 2;
+  let hAngle = hour() * ((2 * Math.PI) / 12) - Math.PI / 2;
+  noFill();
+  strokeWeight(10);
+  stroke("white");
+  arc(
+    width / 2,
+    height / 2,
+    width / 2 + 80,
+    height / 2 + 80,
+    -Math.PI / 2,
+    sAngle,
+    OPEN
+  );
+  strokeWeight(15);
 
-    arc(width/2, height/2, width/2 + 40, height/2 + 40, -Math.PI/2, mAngle, OPEN);
-    strokeWeight(20);
+  arc(
+    width / 2,
+    height / 2,
+    width / 2 + 40,
+    height / 2 + 40,
+    -Math.PI / 2,
+    mAngle,
+    OPEN
+  );
+  strokeWeight(20);
 
-    arc(width/2, height/2, width/2, height/2, -Math.PI/2, hAngle, OPEN);
-
+  arc(width / 2, height / 2, width / 2, height / 2, -Math.PI / 2, hAngle, OPEN);
 }
